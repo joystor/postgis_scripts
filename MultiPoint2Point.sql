c21858cf23bb6fa3887aaf74eb53320a6baf927e
Angel Torres <joys.tower@gmail.com>
2019-03-19 14:43:35 -0600

    Update MultiPoint2Point.sql

--- MultiPoint2Point.sql
+++ MultiPoint2Point.sql
@@ -12,14 +12,14 @@ DECLARE
  _msg TEXT;
 BEGIN
  FOR _recgeoms IN
-    select '"'||f_table_schema||'"."'||f_table_name||'"' tablename, f_geometry_column geomcolumn from geometry_columns where type='MULTIPOINT'
+    select '"'||f_table_schema||'"."'||f_table_name||'"' tablename, f_geometry_column geomcolumn, srid from geometry_columns where type='MULTIPOINT'
  LOOP
    _sql := 'SELECT COUNT(CASE WHEN ST_NumGeometries('||_recgeoms.geomcolumn||') > 1 THEN 1 END) AS multi_geom FROM ' || _recgeoms.tablename;
    raise notice '%', _sql;
    EXECUTE _sql INTO _rcount;
    IF _rcount.multi_geom = 0 THEN
      --_msg = 'Table can export to POINT';
-     _msg = 'ALTER TABLE ' || _recgeoms.tablename || ' ALTER COLUMN '||_recgeoms.geomcolumn||' TYPE geometry(Point,4326) USING ST_GeometryN('||_recgeoms.geomcolumn||', 1)';
+     _msg = 'ALTER TABLE ' || _recgeoms.tablename || ' ALTER COLUMN '||_recgeoms.geomcolumn||' TYPE geometry(Point,'||_recgeoms.srid||') USING ST_GeometryN('||_recgeoms.geomcolumn||', 1);';
    ELSE
      _msg = 'Table can not export to POINT';
    END IF;
